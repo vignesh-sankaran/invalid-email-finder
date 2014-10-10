af5e935a220b9d2366df1a574a19ed5169063e29
Vignesh Sankaran <vignesh.sankaran@me.com>
2014-10-10 20:17:31 +1100

    Added alternative regex statement file

---
+++ compiler.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Author: Vignesh Sankaran
+# Description: This script acts as a compiling script for the C program
+
+# Compile the C program
+gcc -o invalid-email.finder invalid-email-finder.c
