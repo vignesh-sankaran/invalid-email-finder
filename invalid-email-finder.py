8a47a99e18320757cc759472abcaac02f24a91d1
Vignesh <vignesh.sankaran@me.com>
2014-11-21 12:15:40 +1100

    design-documentatin.txt

---
+++ invalid-email-finder.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# 
+import system
+import mailbox
+import message
+
+# Runs the script
+def main():
+
+
+# Find and verify file
+def import_file():
+
+	return null
+
+def verify_directory(directory):
+
+
+if __name__ == "__main__":
+    main()
