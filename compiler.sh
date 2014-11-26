acbdd3da4650593ac5e696a961f102b8c98f5ab7
Vignesh Sankaran <vignesh.sankaran@me.com>
2014-11-27 10:43:05 +1100

    Removed compiler.sh

--- compiler.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Author: Vignesh Sankaran
-# Description: This script acts as a compiling script for the C program
-
-# Compile the C program
-gcc -o invalid-email.finder invalid-email-finder.c
